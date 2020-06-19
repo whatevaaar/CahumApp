019e52bd8d291a5f38660b3cee477525fa2770b5
Whatevaaar <emilianohidalgo314@gmail.com>
2020-06-18 23:34:35 -0500

    Fixed LoginActivity.kt

---
+++ app/src/main/java/com/cahum/cliente/LoginActivity.kt
@@ -0,0 +1,98 @@
+package com.cahum.cliente
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.cahum.cliente.control.DBManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+const val RC_SIGN_IN = 200
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var botonSignIn: SignInButton
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val usuario = firebaseAuth.currentUser
+
+    private val dbManager = DBManager()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        botonSignIn = findViewById(R.id.sign_in_button)
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        botonSignIn.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        if (usuarioLogeado())
+            return
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        if (usuarioLogeado()) {
+            val usuario = FirebaseAuth.getInstance().currentUser
+            dbManager.registrarUsuarioSiNoExiste(usuario!!)
+            redirigirAMenu()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task!!)
+        }
+    }
+
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account!!)
+        } catch (e: ApiException) {
+            Toast.makeText(this, "Error al autenticarse", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        Log.d("LOGIN", "firebaseAuthWithGoogle:" + account.id!!)
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Authentication Completed.", Toast.LENGTH_LONG).show()
+                    redirigirAMenu()
+                }
+                 else Toast.makeText(this, "Authentication Failed.", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    private fun redirigirAMenu(){
+        val intent = Intent(this, MenuActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    fun usuarioLogeado(): Boolean = usuario != null
+
+}
---
+++ app/src/main/java/com/cahum/cliente/MenuActivity.kt
@@ -0,0 +1,36 @@
+package com.cahum.cliente
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId){
+            R.id.menu_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this,LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            R.id.menu_perfil ->{}
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
